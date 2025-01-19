98d91bca9c3346a88351657f20b9d3ddcf175dd2
Rhodan Cervantes <105742277+rhodanrsc@users.noreply.github.com>
2025-01-19 11:38:56 -0700

    working buttons

--- functions.js
+++ functions.js
@@ -1,1 +1,9 @@
- 
+ function butt1() {
+    window.location.href = 'yes.html';
+ }
+ function butt2() {
+    window.location.href = 'no.html';
+ }
+ function back() {
+    window.location.href = 'index.html';
+ }
